c72181bfd9a7bb8f33bb005920482d1fba7b0c93
gs shohel1314 <gsshohel1314@gmail.com>
2021-02-27 22:27:09 +0600

    loops

--- 07_loops.php
+++ 07_loops.php
@@ -1,13 +1,48 @@
 <?php
 
 // while
+/*while(true){
+    echo "Hello"; // Don't print this loop because it is infinity loop. because while condition is always true.
+}*/
 
 // Loop with $counter
+$counter = 0;
+while ($counter < 10) {
+    echo $counter . "<br>";
+    $counter++;
+}
 
 // do - while
+$number = 0;
+do {
+    echo $number . "<br>";
+    $number++;
+} while ($number < 10);
 
 // for
+for ($i=0; $i < 10; $i++) { 
+    echo $i . "<br>";
+}
 
 // foreach
+$fruits = ['Apple', 'Banana', 'Orange', 'Mango'];
+foreach($fruits as $key => $fruit){
+    echo $key .' '. $fruit . '<br>';
+}
 
 // Iterate Over associative array.
+$persons = [
+    'first_name' => 'Shohel',
+    'last_name' => 'Rana',
+    'age' => 25,
+    'channel' => 'Traversy Media',
+    'hobbies' => ['Play Cricket', 'Watch Movies', 'Programming']
+];
+
+foreach ($persons as $key => $person){
+    if(is_array($person)){
+        echo $key .':'. implode(', ', $person) . "<br>";
+    }else{
+        echo $key .":". $person . "<br>";
+    }
+}
